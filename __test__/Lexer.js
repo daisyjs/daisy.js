079fa1af001dcce1e3ccb2976e12d6dfc67dfca7
ImHype <543717080@qq.com>
2017-08-28 22:03:12 +0800

    chore: format test files

--- __test__/Lexer.js
+++ __test__/Lexer.js
@@ -1,145 +1,541 @@
-const {Lexer} = require('../lib/Lexer');
+const {
+    Lexer
+} = require('../lib/Lexer');
+const JSONStringify = str => JSON.stringify(str);
+
 const StringifyLexer = (str, isFormat = 0) => {
     const tokens = Lexer(str);
-    return JSON.stringify(tokens.map(({type, content, isSelfClose}) => ({type, content, isSelfClose})), null, isFormat ? 4: null);
+    return JSONStringify(tokens.map(({
+        type,
+        content,
+        isSelfClose
+    }) => ({
+        type,
+        content,
+        isSelfClose
+    })), null, isFormat ? 4 : null);
 };
-const {expect} = require('chai');
+const {
+    expect
+} = require('chai');
 
-describe('Lexer', function(){
-    it('case - 0', function(){
-        expect(StringifyLexer('<div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"EOF"}]');
+describe('Lexer', function () {
+    it('case - 0', function () {
+        expect(StringifyLexer('<div>')).to.be.equal(JSON.stringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
-    it('case - 1', function(){
-        expect(StringifyLexer('<div >')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"EOF"}]');
-    });
-
-
-
-    it('case - 2', function(){
-        expect(StringifyLexer('<div id>') ).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"EOF"}]');
-    });
-
-
-
-    it('case - 3', function(){
-        expect(StringifyLexer('<div id=>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"EOF"}]');
-    });
-
-
-
-    it('case - 4', function(){
-        expect(StringifyLexer('<div id=1>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"VALUE","content":"1"},{"type":"EOF"}]');
+    it('case - 1', function () {
+        expect(StringifyLexer('<div >')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 5', function(){
-        expect(StringifyLexer('<div></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 2', function () {
+        expect(StringifyLexer('<div id>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 6', function(){
-        expect(StringifyLexer('<div id></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 3', function () {
+        expect(StringifyLexer('<div id=>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 7', function(){
-        expect(StringifyLexer('<div id=></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
-    });
-
-
-    it('case - 8', function(){
-        expect(StringifyLexer('<div id=1></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"VALUE","content":"1"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
-    });
-
-
-
-    it('case - 9', function(){
-        expect(StringifyLexer('<div id="xxxx"></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"VALUE","content":"\\"xxxx\\""},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
-    });
-
-
-
-    it('case - 10', function(){
-        expect(StringifyLexer('<div id=xxxx></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"VALUE","content":"xxxx"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 4', function () {
+        expect(StringifyLexer('<div id=1>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'VALUE',
+            'content': '1'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 11', function(){
-        expect(StringifyLexer('<div id=1 class=2></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"VALUE","content":"1"},{"type":"ATTR","content":"class"},{"type":"VALUE","content":"2"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 5', function () {
+        expect(StringifyLexer('<div></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 12', function(){
-        expect(StringifyLexer('<div id= 1 class= 2></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"VALUE","content":"1"},{"type":"ATTR","content":"class"},{"type":"VALUE","content":"2"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 6', function () {
+        expect(StringifyLexer('<div id></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 13', function(){
-        expect(StringifyLexer('<div id="a 1" class= 2></div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"ATTR","content":"id"},{"type":"VALUE","content":"\\"a 1\\""},{"type":"ATTR","content":"class"},{"type":"VALUE","content":"2"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 7', function () {
+        expect(StringifyLexer('<div id=></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+
+    it('case - 8', function () {
+        expect(StringifyLexer('<div id=1></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'VALUE',
+            'content': '1'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 14', function(){
-        expect(StringifyLexer('{{aaa}}')).to.be.equal('[{"type":"EXPR","content":"aaa"},{"type":"EOF"}]');
+    it('case - 9', function () {
+        expect(StringifyLexer('<div id="xxxx"></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'VALUE',
+            'content': '"xxxx"'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 15', function(){
-        expect(StringifyLexer('<div>a{{aaa}}</div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"TEXT","content":"a"},{"type":"EXPR","content":"aaa"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 10', function () {
+        expect(StringifyLexer('<div id=xxxx></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'VALUE',
+            'content': 'xxxx'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 16', function(){
-        expect(StringifyLexer('<<div>a{{aaa}}</div>')).to.be.equal('[{"type":"TAGNAME","content":"<div"},{"type":"TEXT","content":"a"},{"type":"EXPR","content":"aaa"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 11', function () {
+        expect(StringifyLexer('<div id=1 class=2></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'VALUE',
+            'content': '1'
+        }, {
+            'type': 'ATTR',
+            'content': 'class'
+        }, {
+            'type': 'VALUE',
+            'content': '2'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 17', function(){
-        expect(StringifyLexer('<<div>>{{{aaa}</div>')).to.be.equal('[{"type":"TAGNAME","content":"<div"},{"type":"TEXT","content":">{{{aaa}</div>"},{"type":"EOF"}]');
+    it('case - 12', function () {
+        expect(StringifyLexer('<div id= 1 class= 2></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'VALUE',
+            'content': '1'
+        }, {
+            'type': 'ATTR',
+            'content': 'class'
+        }, {
+            'type': 'VALUE',
+            'content': '2'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 18', function(){
-        expect(StringifyLexer('<d<iv>{{aaa}}</div>')).to.be.equal('[{"type":"TAGNAME","content":"d<iv"},{"type":"EXPR","content":"aaa"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 13', function () {
+        expect(StringifyLexer('<div id="a 1" class= 2></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'id'
+        }, {
+            'type': 'VALUE',
+            'content': '"a 1"'
+        }, {
+            'type': 'ATTR',
+            'content': 'class'
+        }, {
+            'type': 'VALUE',
+            'content': '2'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
 
-    it('case - 19', function(){
+    it('case - 14', function () {
+        expect(StringifyLexer('{{aaa}}')).to.be.equal(JSONStringify([{
+            'type': 'EXPR',
+            'content': 'aaa'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+
+
+    it('case - 15', function () {
+        expect(StringifyLexer('<div>a{{aaa}}</div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'TEXT',
+            'content': 'a'
+        }, {
+            'type': 'EXPR',
+            'content': 'aaa'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+
+
+    it('case - 16', function () {
+        expect(StringifyLexer('<<div>a{{aaa}}</div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': '<div'
+        }, {
+            'type': 'TEXT',
+            'content': 'a'
+        }, {
+            'type': 'EXPR',
+            'content': 'aaa'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+
+
+    it('case - 17', function () {
+        expect(StringifyLexer('<<div>>{{{aaa}</div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': '<div'
+        }, {
+            'type': 'TEXT',
+            'content': '>{{{aaa}</div>'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+
+
+    it('case - 18', function () {
+        expect(StringifyLexer('<d<iv>{{aaa}}</div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'd<iv'
+        }, {
+            'type': 'EXPR',
+            'content': 'aaa'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+
+
+    it('case - 19', function () {
         expect(StringifyLexer(`< d
 
-<iv>{{aaa}}</div>`)).to.be.equal('[{"type":"TEXT","content":" d\\n\\n"},{"type":"TAGNAME","content":"iv"},{"type":"EXPR","content":"aaa"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+<iv>{{aaa}}</div>`)).to.be.equal(JSONStringify([{
+            'type': 'TEXT',
+            'content': '< d\n\n'
+        }, {
+            'type': 'TAGNAME',
+            'content': 'iv'
+        }, {
+            'type': 'EXPR',
+            'content': 'aaa'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
-    it('case - 20', function(){
-        expect(StringifyLexer('<block $if={a=1}></block>')).to.be.equal('[{"type":"TAGNAME","content":"block"},{"type":"ATTR","content":"$if"},{"type":"VALUE","content":"{a=1}"},{"type":"END_TAG","content":"block"},{"type":"EOF"}]');
+    it('case - 20', function () {
+        expect(StringifyLexer('<block $if={a=1}></block>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'block'
+        }, {
+            'type': 'ATTR',
+            'content': '$if'
+        }, {
+            'type': 'VALUE',
+            'content': '{a=1}'
+        }, {
+            'type': 'END_TAG',
+            'content': 'block'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
 
-    it('case - 21', function(){
-        expect(StringifyLexer('<div>{{{aaa}}</div>')).to.be.equal('[{"type":"TAGNAME","content":"div"},{"type":"EXPR","content":"{aaa"},{"type":"END_TAG","content":"div"},{"type":"EOF"}]');
+    it('case - 21', function () {
+        expect(StringifyLexer('<div>{{{aaa}}</div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'EXPR',
+            'content': '{aaa'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
-    it('case - 21', function(){
-        expect(StringifyLexer('<div class="{ a: 1, b: []""></div>')).to.be.equal('');
+    it('case - 22', function () {
+        expect(StringifyLexer('<div class="{ a: 1, b: []""></div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'class'
+        }, {
+            'type': 'VALUE',
+            'content': '"{ a: 1, b: []"'
+        }, {
+            'type': 'ATTR',
+            'content': '"'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
     });
 
+    it('case - 23', function () {
+        expect(StringifyLexer('<div class="{{{\'a\' : 1}}"></</div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'class'
+        }, {
+            'type': 'VALUE',
+            'content': '"{{{\'a\' : 1}}"'
+        }, {
+            'type': 'TEXT',
+            'content': '</'
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+    it('case - 24', function () {
+        expect(StringifyLexer('<div class="{{{\'a\' : 1}}"> {{dsa> 1}}<a /> </div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'class'
+        }, {
+            'type': 'VALUE',
+            'content': '"{{{\'a\' : 1}}"'
+        }, {
+            'type': 'TEXT',
+            'content': ' '
+        }, {
+            'type': 'EXPR',
+            'content': 'dsa> 1'
+        }, {
+            'type': 'TAGNAME',
+            'content': 'a',
+            'isSelfClose': true
+        }, {
+            'type': 'TEXT',
+            'content': ' '
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+    it('case - 25', function () {
+        expect(StringifyLexer('<div class="{{{\'a\' : 1}}"> {{dsa> 1}}< a /> </div>')).to.be.equal(JSONStringify([{
+            'type': 'TAGNAME',
+            'content': 'div'
+        }, {
+            'type': 'ATTR',
+            'content': 'class'
+        }, {
+            'type': 'VALUE',
+            'content': '"{{{\'a\' : 1}}"'
+        }, {
+            'type': 'TEXT',
+            'content': ' '
+        }, {
+            'type': 'EXPR',
+            'content': 'dsa> 1'
+        }, {
+            'type': 'TEXT',
+            'content': '< a /> '
+        }, {
+            'type': 'END_TAG',
+            'content': 'div'
+        }, {
+            'type': 'EOF'
+        }]));
+    });
+
+    it('case - 26', function () {
+        expect(StringifyLexer('<div class="\\d"> {{dsa> 1}}<br> </div>')).to.be.equal(JSONStringify(
+            [{
+                'type': 'TAGNAME',
+                'content': 'div'
+            }, {
+                'type': 'ATTR',
+                'content': 'class'
+            }, {
+                'type': 'VALUE',
+                'content': '"\\d"'
+            }, {
+                'type': 'TEXT',
+                'content': ' '
+            }, {
+                'type': 'EXPR',
+                'content': 'dsa> 1'
+            }, {
+                'type': 'TAGNAME',
+                'content': 'br'
+            }, {
+                'type': 'TEXT',
+                'content': ' '
+            }, {
+                'type': 'END_TAG',
+                'content': 'div'
+            }, {
+                'type': 'EOF'
+            }]
+        ));
+    });
 });
-
-console.log(StringifyLexer(`<div class="{{{'a' : 1}}"></</div>`, true))
-console.log( StringifyLexer( `<div class="{{{'a' : 1}}"> {{dsa> 1}}<a /> </div>`, true ) )
-console.log( StringifyLexer( `<div class="{{{'a' : 1}}"> {{dsa> 1}}< a /> </div>`, true ) )
-console.log( StringifyLexer( `<div class="\\d"> {{dsa> 1}}<br> </div>`, true ) )
--- __test__/Parser.js
+++ __test__/Parser.js
@@ -1,65 +1,67 @@
-const {Parser} = require('../lib/Parser');
+const {
+    Parser
+} = require('../lib/Parser');
 const FormatJSON = (str) => JSON.stringify(str, null, 2);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<div></div>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<div></div>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<img/>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<img/>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<img>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<img>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<img></img>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<img></img>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<div><img a=1>2</div>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<div><img a=1>2</div>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<div a=2></div>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<div a=2></div>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<div>1</div>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<div>1</div>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<block d:if={a}>1</block>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<block d:if={a}>1</block>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<div d:for={list} d:item="item" d:itemIndex="i"></div>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<div d:for={list} d:item="item" d:itemIndex="i"></div>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<div d:for={list} d:for-item="item" d:for-index="i" class="a"></div>')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<div d:for={list} d:for-item="item" d:for-index="i" class="a"></div>')
+    )
+);
 
 console.log(
     FormatJSON(
@@ -74,72 +76,72 @@ console.log(
 </div>
 `)));
 
-// console.log(FormatJSON(Parser(`
-//     <div>
-//         <block d:if={a}>
-//         1
-//         </block>
-//         <block d:elif={b}>
-//         1
-//         </block>
-//         <block d:elif={c}>
-//         1
-//         </block>
-//         <block d:else>
-//         </block>
-//     </div>
-// `)));
+console.log(FormatJSON(Parser(`
+    <div>
+        <block d:if={a}>
+        1
+        </block>
+        <block d:elif={b}>
+        1
+        </block>
+        <block d:elif={c}>
+        1
+        </block>
+        <block d:else>
+        </block>
+    </div>
+`)));
 
-// console.log(
-//     FormatJSON(
-//         Parser(`
-//             <div>
-//                 <block d:for="{list}" d:for-item="item" d:for-index="index">
-//                     {item.type}
-//                 </block>
-//             </div>
-//         `)
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser(`
+            <div>
+                <block d:for="{list}" d:for-item="item" d:for-index="index">
+                    {item.type}
+                </block>
+            </div>
+        `)
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser(`
-//             <Test/>122121
-//         `)
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser(`
+            <Test/>122121
+        `)
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser(`
-//             <!-- a
+console.log(
+    FormatJSON(
+        Parser(`
+            <!-- a
 
-//             aaa
-//         `)
-//     )
-// );
+            aaa
+        `)
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser(`{ a`)
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('{ a')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser(`<a <!-- a -->></a>`)
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<a <!-- a -->></a>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser(`<a></a>`)
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<a></a>')
+    )
+);
 
-// console.log(
-//     FormatJSON(
-//         Parser('<div class=1// />')
-//     )
-// );
+console.log(
+    FormatJSON(
+        Parser('<div class=1// />')
+    )
+);
