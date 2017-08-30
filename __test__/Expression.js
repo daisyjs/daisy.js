2b97b63f9e8e8e595cb7a81a3780bb8b08e34388
ImHype <543717080@qq.com>
2017-08-30 23:36:02 +0800

    chore: add ExpressionTypes

--- __test__/Expression.js
+++ __test__/Expression.js
@@ -3,6 +3,11 @@ const {Expression} = require('../lib/Expression');
 // console.log();
 const result = Expression('1{{ a+1 }}12122121');
 const result1 = Expression('{{ a+1 }}');
+const result2 = Expression('{{ a?1:0 }}');
+const result3 = Expression('a,,,');
 
 console.log(JSON.stringify(result, null, 4));
 console.log(JSON.stringify(result1, null, 4));
+console.log(JSON.stringify(result2, null, 4));
+console.log(JSON.stringify(result3, null, 4));
+
