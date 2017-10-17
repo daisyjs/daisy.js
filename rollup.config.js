725b70570ad7c678565ebc6f2af539aa601cbc93
ImHype <543717080@qq.com>
2017-10-17 23:20:06 +0800

    chore: add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,40 @@
-const commonjs = require('rollup-plugin-commonjs');
-const nodeResolve = require('rollup-plugin-node-resolve');
-const builtins = require('rollup-plugin-node-builtins');
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+import babel from 'rollup-plugin-babel';
 
 const plugins = [
+    babel({
+        exclude: 'node_modules/**'
+    }),
+
     builtins(),
+
     nodeResolve({
         module: true,
         jsnext: true,
         main: true,
-        browser: true,  // Default: false
-        preferBuiltins: false,  // Default: true ,
+        browser: true,
+        preferBuiltins: false,
         customResolveOptions: {
             moduleDirectory: 'node_modules'
         }
     }),
+
     commonjs({
-        // non-CommonJS modules will be ignored, but you can also
-        // specifically include/exclude files
         include: [
             './lib/**',
-        ], // Default: undefined
-        exclude: [], // Default: undefined
-        // these values can also be regular expressions
-        // include: /node_modules/
-
-        // search for files other than .js files (must already
-        // be transpiled by a previous plugin!)
-        extensions: ['.js', '.coffee'], // Default: [ '.js' ]
-
-        // if true then uses of `global` won't be dealt with by this plugin
-        ignoreGlobal: false, // Default: false
-
-        // if false then skip sourceMap generation for CommonJS modules
-        sourceMap: true, // Default: true
-
-        // explicitly specify unresolvable named exports
-        // (see below for more details)
+        ],
+        exclude: [],
+        extensions: ['.js', '.coffee'],
+        ignoreGlobal: false,
+        sourceMap: true,
         namedExports: {
-        }, // Default: undefined
-
-        // sometimes you have to leave require statements
-        // unconverted. Pass an array containing the IDs
-        // or a `id => boolean` function. Only use this
-        // option if you know what you're doing!
+        },
         ignore: ['conditional-runtime-dependency']
     })
 ];
+
 const daisy = {
     name: 'Daisy',
     input: 'src/platforms/browser/index.js',
@@ -61,7 +50,6 @@ const daisy = {
     plugins
 };
 
-
 const serverRender = {
     name: 'Daisy',
     input: 'src/platforms/server/index.js',
@@ -72,7 +60,6 @@ const serverRender = {
         file: 'dist/ssr.js',
         format: ['umd'],
         sourcemap: 'inline',
-        // exports: 'default',
     },
     plugins
 };
@@ -87,11 +74,10 @@ const exampleRender = {
         file: 'examples/dist.js',
         format: ['umd'],
         sourcemap: 'inline',
-        // exports: 'default',
     },
     plugins
 };
 
-module.exports = [
+export default [
     daisy, serverRender, exampleRender
 ];
